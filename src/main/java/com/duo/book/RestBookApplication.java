f2d435accd873b745ccd6b84ef32989de0e5288e
JarnoHilverts <43601359+JarnoHilverts@users.noreply.github.com>
2020-09-23 16:58:18 +0200

    Added profiles, fixed bug with database connection. Postgres and HSQL databases working with different profiles

--- src/main/java/com/duo/book/RestBookApplication.java
+++ src/main/java/com/duo/book/RestBookApplication.java
@@ -1,5 +1,6 @@
 package com.duo.book;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class RestBookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestBookApplication.class, args);
+
     }
 
 }
